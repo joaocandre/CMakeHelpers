b8fcd115405c89c08e72f3a7d3cdc080759ab912
João André <joaocandre@gmail.com>
2023-07-06 12:50:32 +0100

    Update CMakeFetchHelpers.cmake

--- cmake/CMakeFetchHelpers.cmake
+++ cmake/CMakeFetchHelpers.cmake
@@ -73,8 +73,8 @@ function(search_for_package NAME URL BRANCH)
     endif()
     ## set imported flag
     get_target_property(${NAME}_IMPORTED ${${NAME}_TARGET} IMPORTED)
-    # set(${NAME}_FETCHED "ON" CACHE INTERNAL "ON")
-    message(WARNING "${${NAME}_IMPORTED}")
+    set(${NAME}_IMPORTED "${${NAME}_IMPORTED}" CACHE INTERNAL "${${NAME}_IMPORTED}")
+    # message(WARNING "${${NAME}_IMPORTED}")
 
 
 endfunction()
